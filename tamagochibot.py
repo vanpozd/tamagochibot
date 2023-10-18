47fa104f39cf3b679bb1415b6ad6ac4dcf72fd86
ivan <Vanjapozd@gmail.com>
2023-10-18 22:33:08 +0300

    feed function, main markup

--- tamagochibot.py
+++ tamagochibot.py
@@ -2,35 +2,8 @@ import telebot
 import tamconf
 import json
 import datetime
-
-
-bot = telebot.TeleBot(tamconf.main_bot_token)
-
-@bot.message_handler(commands=['start'])
-def welocme(message):
-    echo_all(message)
- 
-def add_data_to_json(jsontype: object, key, value):
-    json.dump(jsontype)
-
-
-
-
-
-
-@bot.message_handler(func=lambda message: True)
-def messagetaker(message):
-    echo_all(
-        
-    )
-
-
-def main():
-    bot.polling()
-
-if __name__ == '__main__':
-    main()
-
+from telebot import types
+#beast - main animal
 
 def echo_all(message):
     chat_id = message.chat.id
@@ -38,3 +11,36 @@ def echo_all(message):
     print(f'Новое сообщение от {message.chat.username}({message.chat.id})({message_date}): {message.text}')
     bot.send_message(1256707116, f"-{message.chat.username}({message.chat.id})\n-({message_date})\n-{message.text}")
     bot.send_message(617493696, f"Новое сообщение от {message.chat.username}({message.chat.id})({message_date}): {message.text}")
+def feed_beast(message):
+    print("shdfnsjdfnjhdsnb")
+def add_data_to_json(jsontype: object, key, value):     #NOT DONE
+    json.dump(jsontype)
+def create_main_markup():
+    main_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    feed_button = types.KeyboardButton("Покорми меня")
+    main_markup.add(feed_button)
+    return main_markup
+
+
+
+bot = telebot.TeleBot(tamconf.main_bot_token)
+
+@bot.message_handler(commands=['start'])
+def welocme(message):
+    echo_all(message)
+    main_markup = create_main_markup()
+    bot.send_message(message.chat.id,"main menu",reply_markup = main_markup)
+
+@bot.message_handler(func=lambda message: True)
+def message_taker(message):
+    echo_all(message)
+    if message.chat.type == 'private':
+        if message.text == "Покорми меня":
+            feed_beast(message)
+
+
+def main():
+    bot.polling()
+
+if __name__ == '__main__':
+    main()
--- tamconf.py
+++ tamconf.py
@@ -1,2 +1,2 @@
 main_bot_token = "6522978019:AAH7LF5gs176VnKCkd1rc-mp6SwYa8xp9wQ"
-data_file = "C:\Users\HP755 G5\Documents\tamabot\tamagochibot\data.json"
+# data_file = "C:\Users\HP755 G5\Documents\tamabot\tamagochibot\data.json"
